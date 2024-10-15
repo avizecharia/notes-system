8ec5d4916f0d0c3fa629aedd4d6980c169a26f18
aviel <aviel469@gmail.com>
2024-10-15 12:19:54 +0300

    register user done

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,23 +12,9 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
-  @Get(':id')
+  @Get('profile')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities/user.interface';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel('User') private userModel: Model<User>) {}
+  async create(createUserDto: CreateUserDto):Promise<User>{
+    try {
+      const hashPass = await bcrypt.hash(createUserDto.password, 10);
+      const newUser = new this.userModel({
+        username: createUserDto.username,
+        password: hashPass,
+        email: createUserDto.email,
+      });
+      return await newUser.save()
+    } catch (err) {
+      console.log(err);
+      throw new BadRequestException(err)
+      
+    }
   }
 
   findAll() {
