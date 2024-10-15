7b71110a23c173ac1bde42c55ba1309e8dd1bae8
aviel <aviel469@gmail.com>
2024-10-15 11:29:54 +0300

    User create intial

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+
+import {IsString,IsEmail,MinLength, isString} from 'class-validator'
+export class CreateUserDto {
+    @IsString()
+    username:string
+    @IsString()
+    @MinLength(6,{message:"password is to sort must be at lest 6 chars"})
+    password:string
+    @IsEmail({},{message:"invalid email format"})
+    email:string
+
+}
