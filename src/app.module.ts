eef56e85c2d4ebb4dfa1ce3fa86ade4be96101b0
aviel <aviel469@gmail.com>
2024-10-15 11:29:26 +0300

    User schema and interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { NotesModule } from './notes/notes.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config'
 @Module({
-  imports: [UserModule, AuthModule, NotesModule],
+  imports: [
+    MongooseModule.forRoot(process.env.CONNECTION_STRING),
+    UserModule,
+    AuthModule,
+    NotesModule,
+  ],
 })
 export class AppModule {}
+
+
